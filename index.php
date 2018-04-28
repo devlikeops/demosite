b5dba24301cb9f0a77d5681e828236b04ab32845
Nikolay <1308@mail.ru>
2018-04-28 23:03:15 +0300

    no message

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
 // Показывать всю информацию, по умолчанию INFO_ALL
-phpinfo();
+//phpinfo();
+echo "Hello world";
 
 // Показывать информацию только о загруженных модулях.
 // phpinfo(8) выдает тот же результат.
-phpinfo(INFO_MODULES);
+//phpinfo(INFO_MODULES);
 
 ?>
